56160b7eb26e1f2a049ad2f41778c5abd52a8c29
WallyHojo <wally.carlson@gmail.com>
2024-11-24 15:57:11 -0800

    second round of updates to dir structure. added images to case studies and reviews.

--- assets/js/case.js
+++ assets/js/case.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         html += '<li class="glide__slide">';
         html += '<div class="card border-0 rounded-2 overflow-hidden">';
         html += '<div class="card-img">';
-        html += `<img src="${item.image}" class="img-fluid">`;
+        html += `<img src="${item.image}" class="img-fluid w-100">`;
         html += '</div>';
         html += '<div class="card-img-overlay d-flex flex-column align-items-center justify-content-center gap-1">';
         html += `<h4 class="mb-0">${item.title}</h4>`;
